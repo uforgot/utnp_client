d61f62821caaec1638cf586c7ae86b01cea4bff6
uforgot <uforgot@me.com>
2025-10-09 00:42:13 +0900

    ably로 수정

--- src/address.ts
+++ src/address.ts
@@ -1,29 +1,30 @@
 import io, { Socket } from 'socket.io-client';
+import * as Ably from 'ably';
 import type InteractiveStage from '@/interactive/interactive.stage.ts';
 import type { DefaultEventsMap } from '@socket.io/component-emitter';
 
 class Address {
   private static instance: Address;
   public interactiveStage: InteractiveStage | undefined;
-  private webSocket: Socket<DefaultEventsMap, DefaultEventsMap> | undefined;
+  // private webSocket: Socket<DefaultEventsMap, DefaultEventsMap> | undefined;
   private data: string | undefined;
 
   private constructor() {}
 
-  private setSocket() {
-    this.webSocket = io('http://192.168.68.65:8087', {
-      transports: ['websocket', 'polling'],
-    });
-
-    this.webSocket.on('connect', this.onConnect.bind(this));
-  }
-
-  onConnect() {
-    console.log('connect');
-    if (!this.webSocket) return;
-
-    this.webSocket.emit('s', this.data);
-  }
+  // private setSocket() {
+  //   this.webSocket = io('http://192.168.68.65:8087', {
+  //     transports: ['websocket', 'polling'],
+  //   });
+  //
+  //   this.webSocket.on('connect', this.onConnect.bind(this));
+  // }
+  //
+  // onConnect() {
+  //   console.log('connect');
+  //   if (!this.webSocket) return;
+  //
+  //   this.webSocket.emit('s', this.data);
+  // }
 
   public static getInstance(): Address {
     if (!Address.instance) {
@@ -34,7 +35,16 @@ class Address {
 
   public setSave() {
     this.data = this.interactiveStage?.setSave();
-    this.setSocket();
+
+    const ably = new Ably.Realtime(
+      'LWMPBA.iyAokg:uUWmR8k2r-li4oPBlT-rz83ju_qSEMMLkd1w2Jxqmig'
+    );
+    ably.connection.once('connected', async () => {
+      console.log('Connected to Ably!');
+      const channel = ably.channels.get('utnp');
+      await channel.publish('send', this.data);
+      ably.connection.close();
+    });
 
     this.interactiveStage?.destroy();
   }
