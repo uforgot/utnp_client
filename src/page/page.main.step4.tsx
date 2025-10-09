b56f43d304fed853cfe158c02c9771806b98cebb
uforgot <uforgot@me.com>
2025-10-09 21:26:20 +0900

    theme color 추가

--- src/page/page.main.step4.tsx
+++ src/page/page.main.step4.tsx
@@ -30,7 +30,7 @@ export default function PageMainStep4() {
         </p>
         <div
           className={
-            'absolute left-1/2 top-1/2 -translate-1/2 mq-[width|850px] rotate-animation'
+            'absolute left-1/2 top-1/2 -translate-1/2 mq-[width|850px] rotate-animation aspect-square'
           }
         >
           <img src={'/loading.svg'} alt={'loading'} />
